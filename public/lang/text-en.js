5da45ff80d6e90db83f2209390216dcd407e6f21
logicseeker <logicseeker@naver.com>
2022-01-28 23:00:00 +0900

    add language pack

---
+++ public/lang/text-en.js
@@ -0,0 +1,33 @@
+const enText = {
+  'type': 'en',
+  'header': {
+    'title': 'Ganyu...',
+    'changeLang': 'change language',
+    'ko': '한국어',
+    'en': 'English'
+  },
+  'video': {
+    'notSupported': 'Video is not supported.'
+  },
+  'introduction': {
+    'who': 'Who is Ganyu?',
+    'quote': `"Only Ganyu's beautifulness is the closest to Heavenly Principles." -- Raiden Shogun`,
+    'intro': `Ganyu is a character appearing in a game 「Genshin Impact」. She is beautifulness itself. I have a lot of words about her but they are inexpressible. So I didn't write those.`,
+    'figcaption': 'Under the setting Sun,<br>Ganyu is nodding off in front of a qingxin flower.'
+  },
+  'ts': {
+    'ts1': 'Traveling Story With Ganyu...',
+    'ts2': `Today I'm going to make a food with Ganyu. Wanmin Restaurant is now busy due to the event but who cares`,
+    'ts3': 'Her special dish is 「Prosperous Peace」. It often appears during cooking 「Universal Peace」.',
+    'ts4': 'Ganyu does it taste good...? If you feel delight, I also feel the same delight too...',
+    'ts5': 'Traveling Story With Ganyu 2...',
+    'ts6': `Today I'm going to take an Enkanomiya adventure with Ganyu. Will she be able to survive among strong monsters here?`,
+    'ts7': '? No way',
+    'ts8': 'Traveling Story End'
+  },
+  'footer': {
+    'greeting': `Hello I'm Ganyu with one constellation owner. Ganyu is the best.`,
+    'githubRepo': 'Github Repository of this website: <a href="https://github.com/logic-finder/Ganyu-Is-Beautiful">https://github.com/logic-finder/Ganyu-Is-Beautiful</a>',
+    'usedFont': 'MapleStory font is used in this website. (<a href="https://maplestory.nexon.com/Media/Font">https://maplestory.nexon.com/Media/Font</a>)'
+  }
+}
---
+++ public/lang/text-ko.js
@@ -0,0 +1,33 @@
+const koText = {
+  'type': 'ko',
+  'header': {
+    'title': '감우...',
+    'changeLang': '언어 변경',
+    'ko': '한국어',
+    'en': 'English'
+  },
+  'video': {
+    'notSupported': '비디오가 지원되지 않습니다.'
+  },
+  'introduction': {
+    'who': '감우가 누구인가요?',
+    'quote': '"오직 감우의 아름다움만이 천리에 가장 가깝도다." -- 라이덴 쇼군',
+    'intro': '감우는 게임 「원신」에 등장하는 캐릭터로 아름다움 그 자체입니다. 쓸 말이 무척 많지만 말로 다 할 수 없습니다. 따라서 적지 않았습니다.',
+    'figcaption': '지는 해 아래에서, 감우가 청심화 앞에서 졸고 있다.'
+  },
+  'ts': {
+    'ts1': '감우와의 여행기...',
+    'ts2': '오늘은 감우와 음식을 만들기로 했어요. 해등절 기간이라 바쁜 만민당의 주방을 점거했어요 뭐어때',
+    'ts3': '감우의 특제 요리는 태평성대에요 세계평화를 조리하면 일정 확률로 나와요',
+    'ts4': '감우야 맛있니... 니가 기쁘면 나도 기쁘다...',
+    'ts5': '감우와의 여행기 2...',
+    'ts6': '오늘은 감우와 연하궁 모험을 떠나기로 했어요. 과연 물몸인 감우가 이곳에서 버틸 수 있을까요?',
+    'ts7': '? 아니 왤케 쌤',
+    'ts8': '여행기 끝'
+  },
+  'footer': {
+    'greeting': '안녕하세요 감우 1돌 보유자입니다. 감우가 최고다',
+    'githubRepo': '이 웹사이트의 깃헙 레포지토리: <a href="https://github.com/logic-finder/Ganyu-Is-Beautiful">https://github.com/logic-finder/Ganyu-Is-Beautiful</a>',
+    'usedFont': '메이플스토리 폰트가 이 웹사이트에서 사용되었습니다. (<a href="https://maplestory.nexon.com/Media/Font">https://maplestory.nexon.com/Media/Font</a>)'
+  }
+}
